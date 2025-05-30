a64e572112815acf5d5c4a856b841ad8ad50fabf
MVerghese <mrinal.verghese@gmail.com>
2025-05-30 15:27:32 -0400

    updated example code

--- OWL.py
+++ OWL.py
@@ -6,50 +6,17 @@ import numpy as np
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 from matplotlib import pyplot as plt
+import cv2
 
+SAM2_PATH = "/home/mverghese/sam2/checkpoints/sam2.1_hiera_large.pt"
 np.random.seed(3)
 
-def show_mask(mask, ax, random_color=False, borders = False):
-	if random_color:
-		color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-	else:
-		color = np.array([30/255, 144/255, 255/255, 0.6])
-	h, w = mask.shape[-2:]
-	mask = mask.astype(np.uint8)
-	mask_image =  mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-	if borders:
-		contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-		# Try to smooth contours
-		contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
-		mask_image = cv2.drawContours(mask_image, contours, -1, (1, 1, 1, 0.5), thickness=2) 
-	ax.imshow(mask_image)
-
-def show_points(coords, labels, ax, marker_size=375):
-	pos_points = coords[labels==1]
-	neg_points = coords[labels==0]
-	ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-	ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-
-def show_box(box, ax):
-	x0, y0 = box[0], box[1]
-	w, h = box[2] - box[0], box[3] - box[1]
-	ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))    
-
-def show_masks(image, masks, scores, point_coords=None, box_coords=None, input_labels=None, borders=True):
-	for i, (mask, score) in enumerate(zip(masks, scores)):
-		plt.figure(figsize=(10, 10))
-		plt.imshow(image)
-		show_mask(mask, plt.gca(), borders=borders)
-		if point_coords is not None:
-			assert input_labels is not None
-			show_points(point_coords, input_labels, plt.gca())
-		if box_coords is not None:
-			# boxes
-			show_box(box_coords, plt.gca())
-		if len(scores) > 1:
-			plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
-		plt.axis('off')
-		plt.show()
+def visualize_mask(img, mask, alpha=0.7, color=(255, 0, 0)):
+	mask_im = np.zeros(img.shape,dtype=np.uint8)
+	mask_im[:,:,:] = color
+	mask_im = cv2.bitwise_and(mask_im,mask_im,mask=mask.astype(np.uint8))
+	disp_im = cv2.addWeighted(img, alpha , mask_im, 1-alpha, 0)
+	return disp_im
 
 class OWLDetector:
 	def __init__(self, model_name="google/owlv2-base-patch16-ensemble", sam_checkpoint=None, device=None):
@@ -132,7 +99,7 @@ def main():
 	img_path = "Test_Image.png"
 	image = Image.open(img_path).convert("RGB")
 	text_labels = ["cutting board", "sponge", "knife"]
-	detector = OWLDetector(sam_checkpoint="/home/mverghese/sam2/checkpoints/sam2.1_hiera_large.pt", device="cuda:1")
+	detector = OWLDetector(sam_checkpoint=SAM2_PATH, device="cuda:0")
 
 	results = detector.inference(image, text_labels)
 	print(results)
@@ -140,6 +107,12 @@ def main():
 
 	cv2.imshow("Detection", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 	cv2.waitKey(0)
+
+	mask = detector.get_obj_mask(image, "cutting board")
+	disp_im = visualize_mask(image, mask)
+	cv2.imshow("Mask", cv2.cvtColor(disp_im, cv2.COLOR_RGB2BGR))
+	cv2.waitKey(0)
+
 
 if __name__ == "__main__":
 	main()
